fee7a24737eb5e76e767e0a3c53caf9607809045
gauravshirkegs <shirkegaurav611@gmail.com>
2023-02-27 23:04:39 +0530

    gaurav portfolio

--- index.js
+++ index.js
@@ -62,5 +62,52 @@ slideshow();
   }
 
   function contact () {
-    window.scrollTo(0,2800);
+    window.scrollTo(0,3190);
+  }
+
+  function resume(){
+    // window.location.href = "https://drive.google.com/file/d/1Nfb2VY46aoj-LEs_8s7z7OEdXuR7_0T7/view?usp=sharing";
+    window.open("https://drive.google.com/file/d/1Nfb2VY46aoj-LEs_8s7z7OEdXuR7_0T7/view?usp=sharing","_blank");
+  }
+
+  const mediaQuerymin = window.matchMedia('(min-width: 820px)')
+  const mediaQuerymax = window.matchMedia('(max-width: 1180px)')
+
+  if(mediaQuerymin.matches&&mediaQuerymax.matches){
+    
+  function about () {
+    window.scrollTo(0,935);
+  }
+
+  function skill () {
+    window.scrollTo(0,1872);
+  }
+    function project () {
+      window.scrollTo(0,2852);
+    }
+  
+    function contact () {
+      window.scrollTo(0,3913);
+    }
+  }else{
+
+    function home () {
+      window.scrollTo(0,0);
+    }
+  
+    function about () {
+      window.scrollTo(0,700);
+    }
+  
+    function skill () {
+      window.scrollTo(0,1332);
+    }
+  
+    function project () {
+      window.scrollTo(0,2032);
+    }
+  
+    function contact () {
+      window.scrollTo(0,3190);
+    }
   }
