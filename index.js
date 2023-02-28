8909d24577b93e6ee9fa91a07d76894243d33e3f
gauravshirkegs <shirkegaurav611@gmail.com>
2023-02-28 21:47:12 +0530

    Gaurav Shirke portpollio

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ slideshow();
     loop: true,
   });
   */
-
+/*
   function home () {
     window.scrollTo(0,0);
   }
@@ -111,3 +111,45 @@ slideshow();
       window.scrollTo(0,3190);
     }
   }
+
+
+
+  // ==================== dropdown menu================
+
+  const mediaQuerymob = window.matchMedia('(min-width: 280px)')
+  const mediaQuerymaxmob = window.matchMedia('(max-width: 820px)')
+
+  if( mediaQuerymob.matches&&mediaQuerymaxmob.matches){
+
+    document.querySelector(".navbar>div>button").style.display=none;
+    document.querySelector(".dropdown").style.display=block;
+
+  function bars() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+}
+*/
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
