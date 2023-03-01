4193794755f3b410761a5e941ef3704a69141d11
gauravshirkegs <shirkegaurav611@gmail.com>
2023-03-01 19:05:02 +0530

    Gaurav Shirke portpollio

--- index.js
+++ index.js
@@ -40,77 +40,12 @@ function openNewWindow(){
 }
 
 
-  function home () {
-    window.scrollTo(0,0);
-  }
-
-  function about () {
-    window.scrollTo(0,700);
-  }
-
-  function skill () {
-    window.scrollTo(0,1332);
-  }
-
-  function project () {
-    window.scrollTo(0,2032);
-  }
-
-  function contact () {
-    window.scrollTo(0,3190);
-  }
-
-  function resume(){
-    // window.location.href = "https://drive.google.com/file/d/1Nfb2VY46aoj-LEs_8s7z7OEdXuR7_0T7/view?usp=sharing";
-    window.open("https://drive.google.com/file/d/1Nfb2VY46aoj-LEs_8s7z7OEdXuR7_0T7/view?usp=sharing","_blank");
-  }
-
-  const mediaQuerymin = window.matchMedia('(min-width: 280px)')
-  const mediaQuerymax = window.matchMedia('(max-width: 600px)')
-
-  if(mediaQuerymin.matches&&mediaQuerymax.matches){
-    let logo = document.querySelector(".logo");
-
-    logo.innerText = "GS";
-  }
-    
-  // function about () {
-  //   window.scrollTo(0,935);
-  // }
-
-  // function skill () {
-  //   window.scrollTo(0,1872);
-  // }
-  //   function project () {
-  //     window.scrollTo(0,2852);
-  //   }
   
-  //   function contact () {
-  //     window.scrollTo(0,3913);
-  //   }
-  // }else{
 
-  //   function home () {
-  //     window.scrollTo(0,0);
-  //   }
   
-  //   function about () {
-  //     window.scrollTo(0,700);
-  //   }
-  
-  //   function skill () {
-  //     window.scrollTo(0,1332);
-  //   }
-  
-  //   function project () {
-  //     window.scrollTo(0,2032);
-  //   }
-  
-  //   function contact () {
-  //     window.scrollTo(0,3190);
-  //   }
-  // }
-
+   function clickbox (){
+    document.getElementById("btn").click();
+  }
 
 
 
